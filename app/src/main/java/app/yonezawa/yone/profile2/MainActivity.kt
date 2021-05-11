d4cedb28efa17c2a886a0336eddf2f9fb143a446
okomesayaka0418 <83440870+okomesayaka0418@users.noreply.github.com>
2021-05-11 23:45:22 +0900

    プロフィールファイル２”

---
+++ app/src/main/java/app/yonezawa/yone/profile2/MainActivity.kt
@@ -0,0 +1,29 @@
+package app.yonezawa.yone.profile2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //画像を見えない状態にする
+       profileImage.isVisible = false
+
+        //テキストを見えない状態にする
+       profileCommentText.isVisible = false
+
+        button1.setOnClickListener {
+
+            //画像を見える状態にする
+            profileImage.isVisible = true
+
+            profileCommentText.isVisible = true
+        }
+
+
+    }
+}
